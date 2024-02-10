7d6663eebdcb4e990ea179b186edf038d1b13f96
michiko lange taguchi <51973693+michi1008@users.noreply.github.com>
2024-02-10 14:32:13 -0800

    added profile

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProfileScreen.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { useProfileMutation } from '../slices/usersApiSlice';
+import { useGetMyOrdersQuery } from '../slices/ordersApiSlice';
 import { setCredentials } from '../slices/authSlice';
+import { FaTimes } from 'react-icons/fa';
 
 const ProfileScreen = () => {
     const [name, setName] = useState('');
@@ -15,6 +18,10 @@ const ProfileScreen = () => {
 
   const { userInfo } = useSelector((state) => state.auth);
 
+  const { data: orders, isLoading, error } = useGetMyOrdersQuery();
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] = useProfileMutation();
+
   useEffect(() => {
     setName(userInfo.name);
     setEmail(userInfo.email);
@@ -23,14 +30,30 @@ const ProfileScreen = () => {
   const dispatch = useDispatch();
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log('sumbmitted')
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+    } else {
+      try {
+        const res = await updateProfile({
+          // NOTE: here we don't need the _id in the request payload as this is
+          // not used in our controller.
+          // _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        toast.success('Profile updated successfully');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
     }
-  
+  }
   return (
     <section className='profile-container'>
         <div className="profile">
             <h2 className='profileTitle'>User Profile</h2>
-            <form className='profileForm' onSubmit={submitHandler}></form>
+            <form className='profileForm' onSubmit={submitHandler}>
             <label>Name</label>
           <input
             type='text'
@@ -66,13 +89,69 @@ const ProfileScreen = () => {
             type='password'
             className='profileInput'
             placeholder='Confirm your password...'
-            id='password'
-            name='password'
+            id='confrimPassword'
+            name='confirmPassword'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
+          <div className='profile-btn-container'>
+          <button type='submit'>Update</button></div>
+          { loadingUpdateProfile && <Loader />}
+          </form>
         </div>
-        <div className="profile-orders"></div>
+        <div className="profile-orders">
+        <h2 className='profileOrder-Title'>User Orders</h2>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <table className='profile-order-table'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th className='details-btn'></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    <Link to={`/order/${order._id}`}>
+                      <button className='btn-sm' variant='light'>
+                        Details
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        </div>
     </section>
   )
 }
